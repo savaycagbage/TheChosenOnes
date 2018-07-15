af1016d66d528a4c1bab6432a01a789f32c6c1d4
savaycagbage <41166572+savaycagbage@users.noreply.github.com>
2018-07-15 13:59:34 +0200

    Create login

---
+++ TheChosenOnes/Assets/Scripts/Client.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour {
+
+    TcpClient client;
+    NetworkStream stream;
+
+
+    public Client(string User, string pass)
+    {
+        Connect("localhost", User + ";" + pass);
+    }
+
+    public void Connect(String server, String message)
+    {
+        try
+        {
+            Int32 port = 13000;
+            client = new TcpClient(server, port);
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", message);
+            data = new Byte[256];
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.WriteLine("\n Press Enter to continue...");
+        Console.Read();
+    }
+
+    public void closeConnection()
+    {
+        // Close everything.
+        stream.Close();
+        client.Close();
+    }
+
+    public bool isConnected()
+    {
+        if(client.Connected)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ TheChosenOnes/Assets/Scripts/Login.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+    public InputField username;
+    public InputField password;
+    public Client client;
+
+    public void getLoginData()
+    {
+        client = new Client(username.text, password.text);
+        if(client.isConnected())
+        {
+            Debug.Log("kiralyok vagyunk!");
+            client.closeConnection();
+            if (!client.isConnected())
+            {
+                Debug.Log("kiralyok vagyunk!");
+        }
+            else
+            {
+                Debug.Log("luzerek vagyunk!");
+            }
+        }
+        else
+        {
+            Debug.Log("luzerek vagyunk!");
+        }
+    }
+}
